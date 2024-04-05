b165afbe2ec16cc21c1a72c40234faa01d465028
rohitprofc <rohitprofc@gmail.com>
2024-04-05 19:24:25 +0530

    Updates

---
+++ precache-manifest.38b5dabb60723a9966eefb23bf4ba585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a28672b62dca536f50de405039ec27",
+    "url": "/boxes/index.html"
+  },
+  {
+    "revision": "0cc3d72998b8e6167784",
+    "url": "/boxes/static/css/2.8ac840ac.chunk.css"
+  },
+  {
+    "revision": "ba16b459ce5f962a806d",
+    "url": "/boxes/static/css/main.1b995af0.chunk.css"
+  },
+  {
+    "revision": "0cc3d72998b8e6167784",
+    "url": "/boxes/static/js/2.99ae3eca.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/boxes/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "ba16b459ce5f962a806d",
+    "url": "/boxes/static/js/main.16183a43.chunk.js"
+  },
+  {
+    "revision": "8557b3f3f34b18258a4d",
+    "url": "/boxes/static/js/runtime~main.b042dc21.js"
+  }
+]);
